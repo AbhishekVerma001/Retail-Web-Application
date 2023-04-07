80aba723302e38a7c6272c9d96e941444ca79d78
Abhishek Verma <abhishek.verma1@stl.tech>
2023-04-07 11:41:03 +0530

    Updated test cases

--- src/test/java/com/ecommerce/RetailWebApplicationTests.java
+++ src/test/java/com/ecommerce/RetailWebApplicationTests.java
@@ -91,145 +91,153 @@ class RetailWebApplicationTests {
 	@Test
 	//@Order(4)
     public void testFindProducts() throws Exception {
-		final String tokenUrl = "http://localhost:8080/auth/token";
-        URI tokenUri = new URI(tokenUrl);
-        auth.setUsername("Abc@gmail.com");
-		auth.setPassword("A1234567");
-         
-        HttpHeaders tokenHeaders = new HttpHeaders();
-        tokenHeaders.set("Content-Type", "application/json");
-        
-        
-        
-        HttpEntity<AuthRequest> tokenRequest = new HttpEntity<>(auth, tokenHeaders);
-         
-        ResponseEntity<JwtResponse> tokenResult = this.restTemplate.postForEntity(tokenUri, tokenRequest, JwtResponse.class);
-        String token=tokenResult.getBody().getToken();
-	
-        System.out.println(token);
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/json");
-        headers.add("Authorization", "Bearer "+token);
-        HttpEntity<Product> request = new HttpEntity<>( headers);
-        
-        //ResponseEntity<Product> result = this.restTemplate.postForEntity(uri, request, UserCredential.class);
-         
-        ResponseEntity<JsonArray> entity = new TestRestTemplate().exchange(
-                "http://localhost:8080/products", HttpMethod.GET, request,
-                JsonArray.class);
-        System.out.println(entity.getBody());
-       
-        Assertions.assertEquals(200, entity.getStatusCode().value());
-        assertNotNull(entity);
+		
+		
+		assertThat(productRepository.findAll()).size().isGreaterThan(0);
+		
+//		final String tokenUrl = "http://localhost:8080/auth/token";
+//        URI tokenUri = new URI(tokenUrl);
+//        auth.setUsername("Abc@gmail.com");
+//		auth.setPassword("A1234567");
+//         
+//        HttpHeaders tokenHeaders = new HttpHeaders();
+//        tokenHeaders.set("Content-Type", "application/json");
+//        
+//        
+//        
+//        HttpEntity<AuthRequest> tokenRequest = new HttpEntity<>(auth, tokenHeaders);
+//         
+//        ResponseEntity<JwtResponse> tokenResult = this.restTemplate.postForEntity(tokenUri, tokenRequest, JwtResponse.class);
+//        String token=tokenResult.getBody().getToken();
+//	
+//        System.out.println(token);
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add("Content-Type", "application/json");
+//        headers.add("Authorization", "Bearer "+token);
+//        HttpEntity<Product> request = new HttpEntity<>( headers);
+//        URI uri = new URI("/products");
+//        //ResponseEntity<JsonArray> result = this.restTemplate.postForEntity(uri, request, JsonArray.class);
+//        
+//        ResponseEntity<JsonArray> entity = new TestRestTemplate().exchange(
+//        		uri, HttpMethod.GET, request,
+//                JsonArray.class);
+//        System.out.println(entity.getBody());
+//       
+//        Assertions.assertEquals(200, entity.getStatusCode().value());
+//        assertNotNull(entity);
     }
 	@Test
 	//@Order(5)
     public void testFindProductsByGender() throws Exception {
+		
+		assertThat(productRepository.findProductsByGender("Men")).size().isGreaterThan(0);
 
-		final String tokenUrl = "http://localhost:8080/auth/token";
-        URI tokenUri = new URI(tokenUrl);
-        auth.setUsername("Abc@gmail.com");
-		auth.setPassword("A1234567");
-         
-        HttpHeaders tokenHeaders = new HttpHeaders();
-        tokenHeaders.set("Content-Type", "application/json");
-        
-        
-        
-        HttpEntity<AuthRequest> tokenRequest = new HttpEntity<>(auth, tokenHeaders);
-         
-        ResponseEntity<JwtResponse> tokenResult = this.restTemplate.postForEntity(tokenUri, tokenRequest, JwtResponse.class);
-        String token=tokenResult.getBody().getToken();
-	
-        System.out.println(token);
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/json");
-        headers.add("Authorization", "Bearer "+token);
-        HttpEntity<Product> request = new HttpEntity<>( headers);
-        
-        //ResponseEntity<Product> result = this.restTemplate.postForEntity(uri, request, UserCredential.class);
-         
-        ResponseEntity<Object> entity = new TestRestTemplate().exchange(
-                "http://localhost:8080/products/search/findProductsByGender?gender=Men", HttpMethod.GET, request,
-                Object.class);
-        
-        System.out.println(entity.getBody());
-        
-        Assertions.assertEquals(200, entity.getStatusCode().value());
-        assertNotNull(entity);
-       
+//		final String tokenUrl = "http://localhost:8080/auth/token";
+//        URI tokenUri = new URI(tokenUrl);
+//        auth.setUsername("Abc@gmail.com");
+//		auth.setPassword("A1234567");
+//         
+//        HttpHeaders tokenHeaders = new HttpHeaders();
+//        tokenHeaders.set("Content-Type", "application/json");
+//        
+//        
+//        
+//        HttpEntity<AuthRequest> tokenRequest = new HttpEntity<>(auth, tokenHeaders);
+//         
+//        ResponseEntity<JwtResponse> tokenResult = this.restTemplate.postForEntity(tokenUri, tokenRequest, JwtResponse.class);
+//        String token=tokenResult.getBody().getToken();
+//	
+//        System.out.println(token);
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add("Content-Type", "application/json");
+//        headers.add("Authorization", "Bearer "+token);
+//        HttpEntity<Product> request = new HttpEntity<>( headers);
+//        
+//        //ResponseEntity<Product> result = this.restTemplate.postForEntity(uri, request, UserCredential.class);
+//         
+//        ResponseEntity<Object> entity = new TestRestTemplate().exchange(
+//                "http://localhost:8080/products/search/findProductsByGender?gender=Men", HttpMethod.GET, request,
+//                Object.class);
+//        
+//        System.out.println(entity.getBody());
+//        
+//        Assertions.assertEquals(200, entity.getStatusCode().value());
+//        assertNotNull(entity);
+//       
     }
 	@Test
 	//@Order(6)
     public void testFindProductsByGenderAndCategory_Id() throws Exception {
 
-		final String tokenUrl = "http://localhost:8080/auth/token";
-        URI tokenUri = new URI(tokenUrl);
-        auth.setUsername("Abc@gmail.com");
-		auth.setPassword("A1234567");
-         
-        HttpHeaders tokenHeaders = new HttpHeaders();
-        tokenHeaders.set("Content-Type", "application/json");
-        
-        
-        
-        HttpEntity<AuthRequest> tokenRequest = new HttpEntity<>(auth, tokenHeaders);
-         
-        ResponseEntity<JwtResponse> tokenResult = this.restTemplate.postForEntity(tokenUri, tokenRequest, JwtResponse.class);
-        String token=tokenResult.getBody().getToken();
-	
-        System.out.println(token);
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/json");
-        headers.add("Authorization", "Bearer "+token);
-        HttpEntity<Product> request = new HttpEntity<>( headers);
-        
-        
-         
-        ResponseEntity<Object> entity = new TestRestTemplate().exchange(
-                "http://localhost:8080/products/search/findProductsByGenderAndCategory_Id?gender=Men&categoryId=3", HttpMethod.GET, request,
-                Object.class);
-        
-        System.out.println(entity.getBody());
-        Assertions.assertEquals(200, entity.getStatusCode().value());
-        assertNotNull(entity);
+		assertThat(productRepository.findProductsByGenderAndCategory_Id("Men",2L)).size().isGreaterThan(0);
+//		final String tokenUrl = "http://localhost:8080/auth/token";
+//        URI tokenUri = new URI(tokenUrl);
+//        auth.setUsername("Abc@gmail.com");
+//		auth.setPassword("A1234567");
+//         
+//        HttpHeaders tokenHeaders = new HttpHeaders();
+//        tokenHeaders.set("Content-Type", "application/json");
+//        
+//        
+//        
+//        HttpEntity<AuthRequest> tokenRequest = new HttpEntity<>(auth, tokenHeaders);
+//         
+//        ResponseEntity<JwtResponse> tokenResult = this.restTemplate.postForEntity(tokenUri, tokenRequest, JwtResponse.class);
+//        String token=tokenResult.getBody().getToken();
+//	
+//        System.out.println(token);
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add("Content-Type", "application/json");
+//        headers.add("Authorization", "Bearer "+token);
+//        HttpEntity<Product> request = new HttpEntity<>( headers);
+//        
+//        
+//         
+//        ResponseEntity<Object> entity = new TestRestTemplate().exchange(
+//                "http://localhost:8080/products/search/findProductsByGenderAndCategory_Id?gender=Men&categoryId=3", HttpMethod.GET, request,
+//                Object.class);
+//        
+//        System.out.println(entity.getBody());
+//        Assertions.assertEquals(200, entity.getStatusCode().value());
+//        assertNotNull(entity);
 
     }
 	
 	@Test
 	//@Order(7)
     public void testFindByNameContaining() throws Exception {
-
-		final String tokenUrl = "http://localhost:8080/auth/token";
-        URI tokenUri = new URI(tokenUrl);
-        auth.setUsername("Abc@gmail.com");
-		auth.setPassword("A1234567");
-         
-        HttpHeaders tokenHeaders = new HttpHeaders();
-        tokenHeaders.set("Content-Type", "application/json");
-        
-        
-        
-        HttpEntity<AuthRequest> tokenRequest = new HttpEntity<>(auth, tokenHeaders);
-         
-        ResponseEntity<JwtResponse> tokenResult = this.restTemplate.postForEntity(tokenUri, tokenRequest, JwtResponse.class);
-        String token=tokenResult.getBody().getToken();
-	
-        System.out.println(token);
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/json");
-        headers.add("Authorization", "Bearer "+token);
-        HttpEntity<Product> request = new HttpEntity<>( headers);
-        
-        //ResponseEntity<Product> result = this.restTemplate.postForEntity(uri, request, UserCredential.class);
-         
-        ResponseEntity<Object> entity = new TestRestTemplate().exchange(
-                "http://localhost:8080/products/search/findByNameContaining?name=casual", HttpMethod.GET, request,
-                Object.class);
-        
-        System.out.println(entity.getBody());
-        Assertions.assertEquals(200, entity.getStatusCode().value());
-        assertNotNull(entity);
+		
+		assertThat(productRepository.findByNameContaining("casual")).size().isGreaterThan(0);
+//		final String tokenUrl = "http://localhost:8080/auth/token";
+//        URI tokenUri = new URI(tokenUrl);
+//        auth.setUsername("Abc@gmail.com");
+//		auth.setPassword("A1234567");
+//         
+//        HttpHeaders tokenHeaders = new HttpHeaders();
+//        tokenHeaders.set("Content-Type", "application/json");
+//        
+//        
+//        
+//        HttpEntity<AuthRequest> tokenRequest = new HttpEntity<>(auth, tokenHeaders);
+//         
+//        ResponseEntity<JwtResponse> tokenResult = this.restTemplate.postForEntity(tokenUri, tokenRequest, JwtResponse.class);
+//        String token=tokenResult.getBody().getToken();
+//	
+//        System.out.println(token);
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add("Content-Type", "application/json");
+//        headers.add("Authorization", "Bearer "+token);
+//        HttpEntity<Product> request = new HttpEntity<>( headers);
+//        
+//        //ResponseEntity<Product> result = this.restTemplate.postForEntity(uri, request, UserCredential.class);
+//         
+//        ResponseEntity<Object> entity = new TestRestTemplate().exchange(
+//                "http://localhost:8080/products/search/findByNameContaining?name=casual", HttpMethod.GET, request,
+//                Object.class);
+//        
+//        System.out.println(entity.getBody());
+//        Assertions.assertEquals(200, entity.getStatusCode().value());
+//        assertNotNull(entity);
     }
 
 
